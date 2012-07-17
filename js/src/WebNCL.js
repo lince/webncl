338ef9b9835adff3705a4e96201a713f8389d678
Alexandre <alex.crondon@gmail.com>
2012-07-17 17:48:33 -0300

    Removed some bugs on the inline player implementation.

--- js/src/WebNCL.js
+++ js/src/WebNCL.js
@@ -371,41 +371,48 @@ WebNclPlayer.playerCount = 0;
 
 $(document).ready(function() {
 
+	var style = '';
+
 	$("nclplayer").each(function() {
 	
 		// todo: demais atributos
 		var src = $(this).attr("src");
 		var id = $(this).attr("id");
 		
-		this.outerHTML = '<div id="'+id+'" class="nclPlayer_'+id+'"> </div>';
+		this.outerHTML = '<div id="'+id+'" class="nclPlayer"> </div>';
 
 		webNclPlayers.push(new WebNclPlayer(src,id));
 		
-		$(".nclPlayer_"+id).css({
-			position: 'relative',
-			left: $(this).attr("left") || '0px',
-			top: $(this).attr("top") || '0px',
-			width: $(this).attr("width") || "854px",
-			height: $(this).attr("height") || "480px"
-		});
+		style +=
+			 '.nclPlayer_' + id + ' {'
+			+'	position: fixed,'
+			+'	left: ' + ($(this).attr('left') || '0px') + ';'
+			+'	top: ' + ($(this).attr('top') || '0px') + ';'
+			+'	width: ' + ($(this).attr('width') || '854px') + ';'
+			+'	height: ' + ($(this).attr('height') || '480px') + ';'
+			+'}';
+
 	});
 	
-	$(".context, #contexts, #settings").css({display: 'none'});
-	
-	$(".player").css({
-		position: 'absolute',
-		left: '0px',
-		top: '0px',
-		overflow: 'hidden',
-		display: 'inline'
-	});
-	
-	$(".playerBkg").css({
-		position: 'absolute',
-		overflow: 'hidden',
-		display: 'none'
-	});
-	
+	style +=
+		 '.context, #contexts, #settings {'
+		+'	display: none;'
+		+'}'
+		+'.player {'
+		+'	position: absolute;'
+		+'	left: 0px;'
+		+'	top: 0px;'
+		+'	overflow: hidden;'
+		+'	display: inline;'
+		+'}'
+		+'.playerBkg {'
+		+'	position: absolute;'
+		+'	overflow: hidden;'
+		+'	display: none;'
+		+'}';
+		
+	$('<style>').text(style).appendTo('head');
+
 });
 
 webNclPlayers = [];
