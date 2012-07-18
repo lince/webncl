802b89763f9c48838c20b5a1b8b60b19f838cb00
Alexandre <alex.crondon@gmail.com>
2012-07-18 10:31:03 -0300

    Bug fix: the player's position was accidentally set to "fixed"

--- js/src/WebNCL.js
+++ js/src/WebNCL.js
@@ -379,19 +379,19 @@ $(document).ready(function() {
 		var src = $(this).attr("src");
 		var id = $(this).attr("id");
 		
-		this.outerHTML = '<div id="'+id+'" class="nclPlayer"> </div>';
-
-		webNclPlayers.push(new WebNclPlayer(src,id));
+		this.outerHTML = '<div id="'+id+'" class="nclPlayer_'+id+'"> </div>';
 		
 		style +=
 			 '.nclPlayer_' + id + ' {'
-			+'	position: fixed,'
+			+'	position: relative;'
 			+'	left: ' + ($(this).attr('left') || '0px') + ';'
 			+'	top: ' + ($(this).attr('top') || '0px') + ';'
 			+'	width: ' + ($(this).attr('width') || '854px') + ';'
 			+'	height: ' + ($(this).attr('height') || '480px') + ';'
 			+'}';
 
+		webNclPlayers.push(new WebNclPlayer(src,id));
+			
 	});
 	
 	style +=
