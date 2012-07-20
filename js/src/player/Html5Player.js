a12d21cb2749ff6f73052dc3890f810a8527f9b1
Erick Melo <erickmelo@gmail.com>
2012-07-20 14:11:36 -0300

    Issue #13 solved.

--- js/src/player/Html5Player.js
+++ js/src/player/Html5Player.js
@@ -148,18 +148,24 @@ Html5Player.prototype.onEndedCallback = function() {
 Html5Player.prototype.load = function(source)
 {
         //erases older content
-        $(this.htmlPlayer).empty();        
+        $(this.htmlPlayer).empty();   
+        
+		var filename = source.substr(0, source.lastIndexOf('.'));
+
         
         //load a new content base on file type
 	switch (this.p.source.type.split("/")[0]) {
 		case "video": {
 			// type = video/*
-			$(this.htmlPlayer).append("<source src='" + source + "'></source>");
+			$(this.htmlPlayer).append("<source type='video/webm' src='" + filename + ".webm'></source>");
+			$(this.htmlPlayer).append("<source type='video/ogg' src='" + filename + ".ogg'></source>");
+			$(this.htmlPlayer).append("<source type='video/mp4' src='" + filename + ".mp4'></source>");
 			break;
 		}
 		case "audio": {
 			// type = audio/*
-			$(this.htmlPlayer).append("<source src='" + source + "'></source>");
+			$(this.htmlPlayer).append("<source type='audio/mpeg' src='" + filename + ".mp3'></source>");
+			$(this.htmlPlayer).append("<source type='audio/ogg' src='" + filename + ".ogg'></source>");
 			break;
 		}
 		case "image": {
