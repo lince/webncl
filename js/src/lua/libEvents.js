4e8725d245307d8561adc26532d50f34f2fc0e88
alexfqc <alessander70@gmail.com>
2012-07-30 10:09:59 -0300

    timer and uptime implemented

--- js/src/lua/libEvents.js
+++ js/src/lua/libEvents.js
@@ -1,5 +1,5 @@
-//TODO: implement the handler as vector
-//TODO: add the position arg for register()
+//TODO: implement the handler as vector ok
+//TODO: add the position arg for register() ok
 //TODO: add the destination arg for post
 
 function libEvents(player){
@@ -8,6 +8,7 @@ function libEvents(player){
 	this.counter = 0;
 	tmp = player.id;
 	this.nodeId = tmp.substring(tmp.indexOf('_')+1);
+	this.t = 0;
 };
 
 libEvents.prototype.register = function(fct){
@@ -32,11 +33,20 @@ libEvents.prototype.post = function(evt){
 		var json = {'class' : 'ncl', 'type' : evt.str['type'], 'action' : evt.str['action'], 'component' : this.nodeId};
 		this.player.postEvent(json);
 	}
-
+	var d = new Date();
+	this.t = d.getTime();
 }
 
-libEvents.prototype.timer = function(t,callback,manager){
+/*libEvents.prototype.timer = function(t,callback,manager){
 	manager.add(t,callback);	
+}*/
+libEvents.prototype.timer = function(timeout, fct){
+	setTimeout(fct, timeout);
+}
 
+libEvents.prototype.uptime = function(){
+	
+	var d = new Date();
+	return(d.getTime() - this.t); 
 }
 
