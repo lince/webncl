e0d40cf04de82e589046344ec653cd4301cee892
caioviel <caioviel@lince-caio>
2012-08-23 13:13:35 -0300

    Broker sample.

---
+++ sample/receiver.lua
@@ -0,0 +1,7 @@
+function on_message(message)
+	print (message)
+end
+
+
+broker.init(http://200.18.98.24:8161/demo/amq)
+broler.register('topic://test', on_message)
---
+++ sample/sender.lua
@@ -0,0 +1,2 @@
+broker.init(http://200.18.98.24:8161/demo/amq)
+broler.post('topic://test', 'testando')
